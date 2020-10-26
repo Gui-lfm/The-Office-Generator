fcf7ca00d8a497c4aed19d6c9149ec3409a25340
GLucena <guilherme.lucena17@gmail.com>
2020-10-26 17:54:57 -0300

    gerador de eps aleatórios criado e atrelado ao botão principal

--- js/temporadas.js
+++ js/temporadas.js
@@ -9,3 +9,20 @@ var temp8 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
 var temp9 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27];
 
 var temporadas = [temp1, temp2, temp3, temp4, temp5, temp6, temp7, temp8, temp9];
+
+var botao = document.querySelector(".botao-principal");
+
+botao.addEventListener("click", function(){
+
+    var tempAleatoria = temporadas[Math.round(Math.random() * (temporadas.length -1))];
+        
+    var epAleatorio = tempAleatoria[Math.round(Math.random() * (tempAleatoria.length -1))];
+
+    var numTemp = (temporadas.indexOf(tempAleatoria) + 1);
+    var numEp = (tempAleatoria.indexOf(epAleatorio) + 1);
+
+    console.log( "temporada Nº " + numTemp);
+    console.log("episódio Nº " + numEp);
+    
+    
+})
